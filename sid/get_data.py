2a8dc0462ad074ed1e7940a95ca6f9f39c7ec1fe
sidShinde <siddhesh@umich.edu>
2017-02-28 17:26:17 -0500

    get X, y output

--- sid/get_data.py
+++ sid/get_data.py
@@ -6,54 +6,57 @@ __all__=['get_data']
 
 def get_data(dataName):
     '''
+    Input:
+    name of the dataset you want to import
+
     Output:
-    returns a list of 3 data-sets scaled to have zero mean and unit variance
+    returns a training matrix 'X' and the prediction vector 'y'
+    where, y: EV/EBITDA
+           X: rest of the matrix
+
+    Usage:
+    X, y = get_data('consumerDiscretionary')
     '''
 
     # read excel files as data-frames:
-    if dataName == consumerDiscretionary or dataName == allTheData :
+    if dataName == 'consumerDiscretionary':
         consumerDiscrete = pd.read_excel('../data/U of M Student Data - Consumer Discretionary .xlsx',\
                                          'Screening', skiprows=7, na_values=['-', 'NM'] )
-        consumerDiscrete = consumerDiscrete.iloc[:, 5::]
-    elif dataName == consumerStaples or dataName == allTheData:
+        dataset = consumerDiscrete.iloc[:, 5::]
+    elif dataName == 'consumerStaples':
         consumerStaples  = pd.read_excel('../data/U of M Student Data - Consumer Staples.xlsx',\
                                          'Screening', skiprows=7, na_values=['-', 'NM'] )
-        consumerStaples  = consumerStaples.iloc[:, 5::]
-    elif dataName == industrials or dataName == allTheData:
-        industrials      = pd.read_excel('../data/U of M Student Data - Industrials.xlsx',\
+        dataset = consumerStaples.iloc[:, 5::]
+    elif dataName == 'industrials':
+        industrials = pd.read_excel('../data/U of M Student Data - Industrials.xlsx',\
                                          'Screening', skiprows=7, na_values=['-', 'NM'] )
-        industrials      = industrials.iloc[:, 5::]
+        dataset = industrials.iloc[:, 5::]
     else:
         raise ValueError('incorrect function input to get_data() ...')
 
-    # list of all the data sets:
-    if dataName == consumerDiscretionary:
-        dataSet = [consumerDiscrete]
-    elif dataName == consumerStaples:
-        dataSet = [consumerStaples]
-    elif dataName == industrials:
-        dataSet = [industrials]
-    elif dataName == allTheData :
-        dataSet = [consumerDiscrete, consumerStaples, industrials]
+    # calculate column means:
+    colMean = np.nanmean(dataset, axis=0)
+    
+    # find indices where you need to replace:
+    inds = np.where( np.isnan( dataset ) )
 
-    for i in range( len(dataSet) ):
-        # calculate column means:
-        colMean = np.nanmean(dataSet[i], axis=0)
+    # convert data-frame to numpy array:
+    data = pd.DataFrame.as_matrix( dataset )
 
-        # find indices where you need to replace:
-        inds = np.where( np.isnan( dataSet[i] ) )
+    # replace NA values with col means:
+    data[inds] = np.take(colMean, inds[1])
 
-        # convert data-frame to numpy array:
-        data = pd.DataFrame.as_matrix( dataSet[i] )
+    # clear variable value:
+    dataset = None
 
-        # replace NA values with col means:
-        data[inds] = np.take(colMean, inds[1])
+    # replace variable with new value:
+    dataset = scale(data)
 
-        # clear variable value:
-        dataSet[i] = None
+    # prediction vector (EV/EBITDA):
+    y = np.diviide( dataset[0], dataset[2] )
 
-        # replace variable with new value:
-        dataSet[i] = scale(data)
+    # training matrix:
+    X = np.delete( dataset, [0, 2], 1 )
 
-    return dataSet
+    return X, y
         
